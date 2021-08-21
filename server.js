7a4a812f6a1dce48f131f7b2fb330b8184fbe992
Tucker Barrett <tuckerbarrett@gmail.com>
2021-08-21 16:42:25 -0400

    Corrects route to notes.html

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const notesData = require('./db/db.json');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Set public folder
 app.use(express.static('public'));
@@ -11,6 +15,10 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.listen(PORT, () => {
