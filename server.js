8975ec17a5b21e31dc0ab3372ad25d8e56dbd169
Tucker Barrett <tuckerbarrett@gmail.com>
2021-08-21 17:56:33 -0400

    Adds incomplete post request in server.js

--- server.js
+++ server.js
@@ -15,12 +15,46 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
+    console.info(`${req.method} request received to get notes`);
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
+
+app.post('/notes', (req, res) => {
+
+    console.info(`${req.method} request received to add a note`);
+    
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid()
+        };
+
+        const response = {
+            status: 'success',
+            body: newNote
+        };
+
+        console.log(response);
+        response.json(response);
+        
+    } else {
+        res.json('Error in adding note');
+    }
+
+
+});
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
